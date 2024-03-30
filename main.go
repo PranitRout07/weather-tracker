0f19bdf42417716908ad31934cd84320053f1865
PranitRout07 <pranitrout72@gmail.com>
2024-03-30 08:02:12 +0530

    updated code

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
 	"github.com/rs/cors"
 )
+
 
 type WeatherInfo struct {
 	Main        string `json:"main"`
@@ -16,45 +18,48 @@ type WeatherInfo struct {
 }
 
 type GetWeatherData struct {
-	Name    string       `json:"name"`
-	Main    struct {
+	Name string `json:"name"`
+	Main struct {
 		Kelvin float64 `json:"temp"`
 	} `json:"main"`
+
 	Weather []WeatherInfo `json:"weather"`
 }
 
-func query(city string, apiToken string) (GetWeatherData, error) {
-	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiToken)
+
+func querry(city string) (GetWeatherData, error) {
+	apiToken := os.Getenv("API_TOKEN")
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiToken)
 	if err != nil {
 		return GetWeatherData{}, err
 	}
+	fmt.Println("response",resp.Body)
 	defer resp.Body.Close()
-	var data GetWeatherData
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+	var d GetWeatherData
+	json.NewDecoder(resp.Body).Decode(&d)
+	d.Main.Kelvin = d.Main.Kelvin -273
+	d.Main.Kelvin = float64(int(d.Main.Kelvin*100)) / 100
+	if err != nil {
 		return GetWeatherData{}, err
 	}
-	data.Main.Kelvin -= 273.15 
-	return data, nil
+	return d, nil
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
-		city := strings.TrimPrefix(r.URL.Path, "/weather/")
-		apiToken := os.Getenv("API_TOKEN")
-		data, err := query(city, apiToken)
-		if err != nil {
-			http.Error(w, "Error fetching weather data: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		if len(data.Weather) == 0 {
-			http.Error(w, "Weather data not found for the city", http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(data)
-	})
+	mux.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := querry(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+
 
 	handler := cors.Default().Handler(mux)
 
